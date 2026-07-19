28a0a26e28e7baa652dcf213701f8128828704d6
Sitharini Vihasna <sitharinivihasna2004@gmail.com>
2026-07-19 23:35:30 +0530

    Initial commit - Hello World Maven project

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+import static com.diogonunes.jcolor.Ansi.colorize;
+import static com.diogonunes.jcolor.Attribute.*;
+
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(colorize("Green text", GREEN_TEXT()));
+        System.out.println(colorize("Red on White", RED_TEXT(), WHITE_BACK()));
+        System.out.println("Hello World!");
+    }
+}
